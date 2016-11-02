14e4fd917f0537bd5104efb2034f8ff033c5b6f4
olivier marie-joseph <olivier@donpadre.fr>
2016-11-02 15:27:34 +0100

    Automated commit at 2016-11-02 14:27:34 UTC by middleman-deploy 2.0.0-alpha

--- javascripts/all.js
+++ javascripts/all.js
@@ -1,1 +1,63 @@
-$(function(){var i={init:function(){$("#portfoliolist").mixItUp({selectors:{target:".folio",filter:".filter"},load:{filter:".app, .card, .icon, .ill, .logotype, .web"}})}};i.init()});
+
+$(function () {
+
+	var filterList = {
+
+		init: function () {
+
+			// MixItUp plugin
+			// http://mixitup.io
+			$('#portfoliolist').mixItUp({
+				selectors: {
+  			  target: '.folio',
+  			  filter: '.filter'
+  		  },
+  		  load: {
+    		  filter: '.app, .card, .icon, .ill, .logotype, .web' // show app tab on first load
+    		}
+			});
+
+		}
+
+	};
+
+	// Run the show!
+	filterList.init();
+
+});
+/////////////////////// Portfolio fin //////////////////////////////
+
+///////////////////// Compétence developpement  debut ////////////////////////
+  $( document ).ready(function() {
+  // Main variables
+    var $aboutTitle = $('.about-myself .content h2');
+    var $developmentWrapper = $('.development-wrapper');
+    var developmentIsVisible = false;
+    var middle_of_developmentWrapper = $developmentWrapper.offset().top + $developmentWrapper.outerHeight()/2;
+
+    if((bottom_of_window > middle_of_developmentWrapper)&& (developmentIsVisible == false)){
+
+      $('.skills-bar-container li').each( function(){
+
+        var $barContainer = $(this).find('.bar-container');
+        var dataPercent = parseInt($barContainer.data('percent'));
+        var elem = $(this).find('.progressbar');
+        var percent = $(this).find('.percent');
+        var width = 0;
+
+        var id = setInterval(frame, 15);
+
+        function frame() {
+          if (width >= dataPercent) {
+              clearInterval(id);
+          } else {
+            width++;
+            elem.css("width", width+"%");
+            percent.html(width+" %");
+          }
+        }
+      });
+      developmentIsVisible = true;
+    }
+  });
+///////////////////// Compétence developpement  fin ////////////////////////
--- javascripts/social.js
+++ javascripts/social.js
@@ -1,1 +1,52 @@
-!function(){var e=function(e,t,n,r){var o=n||640,i=r||320,c=window.screenLeft||window.screenX,a=window.screenTop||window.screenY,u=window.innerWidth||document.documentElement.clientWidth,d=window.innerHeight||document.documentElement.clientHeight,l=c+u/2-o/2,s=a+d/2-i/2,w=window.open(e,t,"scrollbars=yes, width="+o+", height="+i+", top="+s+", left="+l);return w.focus(),!0};document.querySelector(".share_twitter").addEventListener("click",function(t){t.preventDefault();var n=this.getAttribute("data-url"),r="https://twitter.com/intent/tweet?text="+encodeURIComponent(document.title)+"&via=don_padre&url="+encodeURIComponent(n);e(r,"Partager sur Twitter")}),document.querySelector(".share_facebook").addEventListener("click",function(t){t.preventDefault();var n=this.getAttribute("data-url"),r="https://www.facebook.com/sharer/sharer.php?u="+encodeURIComponent(n);e(r,"Partager sur facebook")}),document.querySelector(".share_gplus").addEventListener("click",function(t){t.preventDefault();var n=this.getAttribute("data-url"),r="https://plus.google.com/share?url="+encodeURIComponent(n);e(r,"Partager sur Google+")}),document.querySelector(".share_linkedin").addEventListener("click",function(t){t.preventDefault();var n=this.getAttribute("data-url"),r="https://www.linkedin.com/shareArticle?url="+encodeURIComponent(n);e(r,"Partager sur Linkedin")})}();
+
+(function(){
+
+    var popupCenter = function(url, title, width, height){
+        var popupWidth = width || 640;
+        var popupHeight = height || 320;
+        var windowLeft = window.screenLeft || window.screenX;
+        var windowTop = window.screenTop || window.screenY;
+        var windowWidth = window.innerWidth || document.documentElement.clientWidth;
+        var windowHeight = window.innerHeight || document.documentElement.clientHeight;
+        var popupLeft = windowLeft + windowWidth / 2 - popupWidth / 2 ;
+        var popupTop = windowTop + windowHeight / 2 - popupHeight / 2;
+        var popup = window.open(url, title, 'scrollbars=yes, width=' + popupWidth + ', height=' + popupHeight + ', top=' + popupTop + ', left=' + popupLeft);
+        popup.focus();
+        return true;
+    };
+
+    document.querySelector('.twitter').addEventListener('click', function(e){
+        e.preventDefault();
+        // var url = this.getAttribute('data-url');
+        var url = document.URL;
+        var shareUrl = "https://twitter.com/intent/tweet?text=" + encodeURIComponent(document.title) +
+            "&via=don_padre" +
+            "&url=" + encodeURIComponent(url);
+        popupCenter(shareUrl, "Partager sur Twitter");
+    });
+
+    document.querySelector('.facebook').addEventListener('click', function(e){
+        e.preventDefault();
+        // var url = this.getAttribute('data-url');
+        var url = document.URL;
+        var shareUrl = "https://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent(url);
+        popupCenter(shareUrl, "Partager sur facebook");
+    });
+
+    document.querySelector('.google-plus').addEventListener('click', function(e){
+        e.preventDefault();
+        // var url = this.getAttribute('data-url');
+        var url = document.URL;
+        var shareUrl = "https://plus.google.com/share?url=" + encodeURIComponent(url);
+        popupCenter(shareUrl, "Partager sur Google+");
+    });
+
+    document.querySelector('.linkedin').addEventListener('click', function(e){
+        e.preventDefault();
+        // var url = this.getAttribute('data-url');
+        var url = document.URL;
+        var shareUrl = "https://www.linkedin.com/shareArticle?url=" + encodeURIComponent(url);
+        popupCenter(shareUrl, "Partager sur Linkedin");
+    });
+
+})();
